fb519ebbe8badb91e15d0eeb53c1bc229cc3a8a8
tonny <adventonny@gmail.com>
2018-05-23 09:13:47 +0700

    ganti index

--- pages/index.vue
+++ pages/index.vue
@@ -20,14 +20,7 @@
         Personal Website
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+       
       </div>
     </div>
   </section>
